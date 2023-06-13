702d9ce45b1966f0c3e13320dab65b42187b78b5
mirza <36728610+abusaeedmirza@users.noreply.github.com>
2023-06-13 12:36:32 +0000

    Update 14 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var R1 = document.getElementById("row1");
+var R2 = document.getElementById("row2");
+var R3 = document.getElementById("row3");
+const rows = [R1, R2, R3];
+const rowdata = [
+  ["Webmaster", "example1@email.com", "Helsinginkatu 3", "X"],
+  ["User123", "example2@email.com", "Oulunkatu 1", "-"],
+  ["AnotherUser222", "example3@email.com", "Vaasankatu 2", "-"],
+];
+for (let i = 0; i < rowdata.length; i++) {
+  for (let x = 0; x < rowdata[i].length; x++) {
+    var cell = document.createElement("td");
+    var txt = document.createTextNode(rowdata[i][x]);
+    cell.appendChild(txt);
+    rows[i].appendChild(cell);
+  }
+}
